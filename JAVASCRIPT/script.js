d24882ca0b2b6d30dbe2accc9bb3b899ad570b4e
Aytac-Ehmedzade <ehmedzadeaytac0610@gmail.com>
2023-07-28 14:07:31 +0400

    new

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,111 @@
+// let var Const
+
+//let number = 5
+//console.log(number)
+
+//var text='myText';
+//console.log(text)
+
+
+//const pi =3.14
+//console.log(pi);
+
+
+//Data Types
+
+//Number
+
+//let number=5;
+
+//console.log(typeof number)
+
+//String
+
+//let text='lorem';
+//console.log(typeof text);
+
+//BigInt
+//let myNumber=7n;
+//console.log(typeof myNumber)
+
+//Boolean
+//let example=4>1;
+//console.log(example)
+
+//Null
+// let users=null;
+// console.log(typeof users)
+
+//Underfined
+// let number;
+// console.log(number)
+
+// Object
+// Math
+
+//Symbol
+
+
+//alert('Salam')
+
+// prompt('adinizi qeyd edin')
+
+//confirm('davam etmek isteyirsiz ')
+
+// let addName=prompt('adinizi qeyd edin')
+// console.log(addName)
+
+// let exam=confirm('imtahan; bitirmek isteyirsini?')
+// console.log(exam)
+
+//console.log(2**8)
+//console.log(8%3)
+
+// let s="my "+"string";
+// console.log(s)
+
+// let a=5;
+// let b=7;
+// console.log(a*b)
+
+//console.log('1'+2)
+// console.log(1+'5')
+// console.log('hello '+'world')
+
+//console.log(4+'5'+101)//45101
+
+//console.log(101-'11'+1)//91
+
+//  console.log(5+'2'-'1'**2);
+//increment
+
+// let a=6;
+// a++;
+// console.log(a);
+
+// let age=18;
+// if(age>30){
+//     console.log('yasiniz 18den coxdur')
+// }else{
+//     console.log('yasiniz 18den azdir')
+// }
+
+let addName = prompt('adinizi qeyd edin')
+console.log('welcome '+ addName)
+
+let a =prompt ('birinci ededi daxil edin');
+let b =prompt('ikinci ededi daxil edin');
+if(a>b){
+    console.log(a)
+}
+else{
+    console.log(b)
+}
+let number = prompt('eded daxil edin');
+if(number>0){
+    console.log('eded musbetdir')
+}
+else{
+    console.log('eded menfidir')
+}
+ 
