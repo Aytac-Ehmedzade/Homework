fafd6e62004e550f27fc0b0d4d1264dad3868c13
Aytac-Ehmedzade <ehmedzadeaytac0610@gmail.com>
2023-08-22 14:34:44 +0400

    new

---
+++ JAVASCRIPT(4)/script.js
@@ -0,0 +1,8 @@
+let a = prompt('Select box to animator:\n1.red \n2.green \n3.blue')
+console.log(a);
+let b = prompt('select animation: 1)kvadrat dovre 2)firlanan boyume 3)kvadrat donen dovre 4)hamisi');
+console.log(b);
+let c = prompt('nece saniye')
+function timepromt(){
+
+} 
---
+++ JAVASCRIPT(5)/script.js
@@ -0,0 +1,14 @@
+document.querySelector('#button').onclick = function(){
+       
+}
+let a = document.querySelector('#textadd').innerHTML
+  
+    for(i>a.length; i++){
+        if(value.lenght>0){
+            console.log('fine');    
+        }
+        else{
+            alert("add text")
+        }
+    
+    }
