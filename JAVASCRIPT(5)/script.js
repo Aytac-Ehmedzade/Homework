0c0248bfe527d139b83cc5e7aa335dff32fe31ba
Aytac-Ehmedzade <ehmedzadeaytac0610@gmail.com>
2023-08-31 23:39:58 +0400

    new

--- JAVASCRIPT(5)/script.js
+++ JAVASCRIPT(5)/script.js
@@ -1,12 +1,24 @@
-document.querySelector('#button').onclick = function(){
-       
+document.querySelector('#textadd').onclick = function(){
+    let input = document.querySelector('#textadd');
+    if(input.value.lenght > 0){
+        let container = document.querySelector('#container');
+        let newItem = document.createElement('div');
+        newItem.innerHTML += `
+        <div class="main">
+            <span class = "inputtext">${input.value}</span>
+            <div class = "buttons">
+                <button class = "deleteBtn">Delete</button>
+                <button class = "editBtn">Edit</button>
+            </div>
+        </div>
+        `;
+        let deleteBtn = newItem.querySelector('.deleteBtn');
+        deleteBtn.onclick = function(){
+            container.removeChild(newItem);
+        }
+        container.appendChild(newItem);
+        input.value = '';
+    } else{
+        alert("please enter text");
+    }
 }
-let a = document.querySelector('#textadd').innerHTML
-    if(value.lenght>0){
-        console.log('fine');    
-    }
-    else{
-        alert("add text")
-    }
-
-    for(i>a.length i++)
