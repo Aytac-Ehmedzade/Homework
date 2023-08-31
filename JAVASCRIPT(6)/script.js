395742959294d29686e87cad248a972b17510761
Aytac-Ehmedzade <ehmedzadeaytac0610@gmail.com>
2023-08-31 23:01:28 +0400

    new

--- JAVASCRIPT(6)/script.js
+++ JAVASCRIPT(6)/script.js
@@ -1,24 +1,25 @@
-// let promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50));
-// let promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30));
+ //usul 1
+promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50));
+promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30));
 
-// Promise.all([promise1, promise2]).then((values) => {
-//      function Promise(promise1,promise2) {
-//      return promise1 + promise2 ;}
-//     console.log(values);    
-// })
+Promise.all([promise1, promise2]).then((value)=>{                            
+    let sum = value.reduce((a,b)=>{
+        return a+b;})
+    console.log(sum);})
 
-function promise1() {
+//usul2
+function promiseX() {
     return new Promise(resolve => {setTimeout(() => resolve(5), 1000);});
 }
 
-function promise2() {
+function promiseY() {
     return new Promise(resolve => {setTimeout(() => resolve(3), 2000);});
 }
 
 function asyncSum() {
     return new Promise(resolve => {
-        const xPromise = promise1();
-        const yPromise = promise2();
+        const xPromise = promiseX();
+        const yPromise = promiseY();
         Promise.all([xPromise, yPromise]).then(([x, y]) => {
             resolve(x + y);
         });
